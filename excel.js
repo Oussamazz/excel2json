a2624b07dd2c86f59b42986031e55d72f5f7ed89
The_Quieter <oussama.azzouzi9@gmail.com>
2022-04-03 04:02:54 +0000

    fix multi files

--- excel.js
+++ excel.js
@@ -15,14 +15,26 @@ document.getElementById('button').addEventListener("click", () => {
         let fileReader = new FileReader();
         fileReader.readAsBinaryString(selectedFile);
         fileReader.onload = (event)=>{
-         data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
+        data = event.target.result;
+        console.log(data);
+        // var cfb = XLS.CFB.read(data, {type: 'binary'});
+        // var workbook = XLS.parse_xlscfb(cfb);
+        let workbook = XLSX.read(data,{type:"binary"});
+        console.log(workbook);
+        // var Sheet = workbook.SheetNames[0];
+        // var excelRows = XLS.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
+        // console.log(excelRows);
+        workbook.SheetNames.forEach(element => {
+            let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[element]);
+            document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
+
+            console.log(rowObject);
+        });
+        data = null;
+        //  workbook.SheetNames.forEach(sheet => {
+        //       let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+        //       console.log(rowObject);
+        //  });
         }
     }
 });
